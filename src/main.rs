f40b4a14cbf3ed5405afebf649721e6a72a9a81a
Olivier Ansaldi <oansaldi@gmail.com>
2018-12-07 02:53:25 +0000

    Day 2.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 
 fn day1_part1(input: &str) -> i32 {
@@ -19,10 +20,55 @@ fn day1_part2(input: &str) -> i32 {
     panic!("Should not be reached");
 }
 
+fn day2_part1(input: &str) -> i32 {
+    let mut ids_with_duplicate_letter = 0;
+    let mut ids_with_triplicate_letter = 0;
+    for l in input.lines() {
+        let mut letter_counts = HashMap::new();
+        for c in l.chars() {
+            let counter = letter_counts.entry(c).or_insert(0);
+            *counter += 1;
+        }
+        if letter_counts.values().any(|&count| count == 2) {
+            ids_with_duplicate_letter += 1;
+        }
+        if letter_counts.values().any(|&count| count == 3) {
+            ids_with_triplicate_letter += 1;
+        }
+    }
+    return ids_with_duplicate_letter * ids_with_triplicate_letter;
+}
+
+fn day2_part2(input: &str) -> String {
+    // In this map, the key is a candidate string for the solution, and the value is the string
+    // we derived it from.
+    let mut candidates = HashMap::new();
+    for l in input.lines() {
+        for i in 0..l.len() {
+            let candidate = l.chars()  // Iterate over all characters
+                .enumerate()  // Add index
+                .filter(|(pos, _)| *pos != i)  // Keep all chars except the one at index i
+                .map(|(_, c)| c)  // Drop the index
+                .collect::<String>();  // Reassemble the string
+            let matching_line = candidates.entry(candidate.clone()).or_insert(l);
+            // Avoid matching with itself. This occurs for consecutive repeated characters.
+            // e.g.  "aa"
+            if *matching_line != l {
+                return candidate;
+            }
+        }
+    }
+    panic!("No solutions!");
+}
+
 fn main() {
     println!("# day 1");
     println!("part 1: {}", day1_part1(include_str!("day1.input")));
     println!("part 2: {}", day1_part2(include_str!("day1.input")));
+
+    println!("# day 2");
+    println!("part 1: {}", day2_part1(include_str!("day2.input")));
+    println!("part 2: {}", day2_part2(include_str!("day2.input")));
 }
 
 #[cfg(test)]
@@ -42,5 +88,16 @@ mod tests {
         assert_eq!(day1_part2("+3\n+3\n+4\n-2\n-4"), 10);
         assert_eq!(day1_part2("-6\n+3\n+8\n+5\n-6"), 5);
         assert_eq!(day1_part2("+7\n+7\n-2\n-7\n-4"), 14);
+    }
+
+    #[test]
+    fn test_day2_part1() {
+        assert_eq!(day2_part1("abcdef\nbababc\nabbcde\nabcccd\naabcdd\nabcdee\nababab"), 12);
+    }
+
+    #[test]
+    fn test_day2_part2() {
+        assert_eq!(day2_part2("abcde\nfghij\nklmno\npqrst\nfguij\naxcye\nwvxyz").as_str(), "fgij");
+        assert_eq!(day2_part2("aa\nbc\nbd").as_str(), "b");
     }
 }
